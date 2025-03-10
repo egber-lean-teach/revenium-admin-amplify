0f99be4d84814c9d274122e5d0696325fc87c281
egber ensignares <egber.ensignares.revenium.io>
2025-03-10 13:03:07 -0500

    feat: Add categories, subcategories

--- src/app/(main)/(private)/dashboard/help_text/page.tsx
+++ src/app/(main)/(private)/dashboard/help_text/page.tsx
@@ -1,23 +1,18 @@
 import TextService from "@/app/infrastructure/services/text.service";
 import DashboardSectionOrganisms from "@/app/ui/organisms/DashboardSection.organisms";
+import { revalidatePath } from "next/cache";
 
 export default async function Help_textView() {
   try {
     const texts = await TextService.getTexts();
-    console.log("texts", texts);
+    const categories = await TextService.getCategories();
+    const subcategories = await TextService.getSubcategories();
+    revalidatePath("/dashboard/help_text");
     return (
       <DashboardSectionOrganisms
-        categories={["category1"]}
-        response={
-          texts.data
-            ? texts
-            : {
-                data: {},
-                message: "No data available",
-                statusCode: 404,
-              }
-        }
-        subcategories={["subcategory1"]}
+        categories={categories}
+        response={texts}
+        subcategories={subcategories}
       />
     );
   } catch (error: unknown) {
