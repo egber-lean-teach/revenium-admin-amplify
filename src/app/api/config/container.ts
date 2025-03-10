967b13393195124d646e0000d9e95d4f2b3774d5
egber ensignares <egber.ensignares.revenium.io>
2025-03-10 12:49:39 -0500

    fix

--- src/app/api/config/container.ts
+++
@@ -1,6 +0,0 @@
-import { container } from "tsyringe";
-import TextService from "../services/texts.service";
-import TextRepository from "../repositories/texts.repository";
-
-container.registerSingleton<TextService>(TextService);
-container.registerSingleton<TextRepository>(TextRepository);
