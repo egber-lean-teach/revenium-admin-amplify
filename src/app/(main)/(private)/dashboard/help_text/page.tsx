0b87e0270c4785e0b0d51c2812f24df83c8bf494
egber ensignares <egber.ensignares.revenium.io>
2025-03-10 09:46:27 -0500

    feat: Add Help_textView

--- src/app/(main)/(private)/dashboard/help_text/page.tsx
+++ src/app/(main)/(private)/dashboard/help_text/page.tsx
@@ -1,3 +1,23 @@
+import DashboardSectionOrganisms from "@/app/ui/organisms/DashboardSection.organisms";
+
 export default function Help_textView() {
-  return <div>Help text view</div>;
+  return (
+    <DashboardSectionOrganisms
+      categories={["category1"]}
+      response={{
+        message: "",
+        statusCode: 0,
+        data: {
+          id1: {
+            category: "category1",
+            subcategory: "subcategory1",
+            name: "name1",
+            id: "id1",
+            description: "description1",
+          },
+        },
+      }}
+      subcategories={["subcategory1"]}
+    />
+  );
 }
