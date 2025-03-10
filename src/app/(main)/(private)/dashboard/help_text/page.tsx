d6b5d0c30042243ace0b7b52a585da77a79d9014
egber ensignares <egber.ensignares.revenium.io>
2025-03-10 17:16:26 -0500

    fix: solving problem

--- src/app/(main)/(private)/dashboard/help_text/page.tsx
+++ src/app/(main)/(private)/dashboard/help_text/page.tsx
@@ -1,40 +1,38 @@
+import { Metadata, ResolvingMetadata } from "next";
 import TextService from "@/app/infrastructure/services/text.service";
 import ProviderPagination from "@/app/ProvidePagination";
 import DashboardSectionOrganisms from "@/app/ui/organisms/DashboardSection.organisms";
 
-interface IHelp_textViewProps {
-  searchParams: {
-    page?: string;
-    totalPage?: string;
-    name?: string;
-  };
-}
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
-export const generateMetadata = async ({
-  searchParams,
-}: IHelp_textViewProps): Promise<{ title: string; description: string }> => {
-  const page: number = parseInt(searchParams?.page || "1");
-
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  console.log("parent", parent);
+  const searchParamsResolved = await searchParams;
+  const page = parseInt((searchParamsResolved.page as string) || "1");
   return {
     title: `Help Texts - Page ${page}`,
     description: `Revenium - Admin`,
   };
-};
+}
 
-export default async function Help_textView({
-  searchParams,
-}: IHelp_textViewProps) {
+export default async function Help_textView({ searchParams }: Props) {
   try {
-    const page: number = searchParams.page ? parseInt(searchParams.page) : 1;
-    const size: number = searchParams.totalPage
-      ? parseInt(searchParams.totalPage)
+    const searchParamsResolved = await searchParams;
+    const page: number = searchParamsResolved.page
+      ? parseInt(searchParamsResolved.page as string)
+      : 1;
+    const size: number = searchParamsResolved.totalPage
+      ? parseInt(searchParamsResolved.totalPage as string)
       : 3;
-
     const texts = await TextService.getTexts(page, size);
     const categories = await TextService.getCategories();
     const subcategories = await TextService.getSubcategories();
-
-    console.log("texts", texts.length);
 
     if (!texts || !categories || !subcategories) {
       return <div>No data available.</div>;
