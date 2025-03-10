1ea936da20b1759e895ee8b359d7f6915541aab0
egber ensignares <egber.ensignares.revenium.io>
2025-03-10 11:17:40 -0500

    .

--- src/app/(main)/(private)/dashboard/help_text/page.tsx
+++ src/app/(main)/(private)/dashboard/help_text/page.tsx
@@ -5,26 +5,14 @@ export default async function Help_textView() {
   try {
     const texts = await TextService.getTexts();
     console.log("texts", texts);
+    return (
+      <DashboardSectionOrganisms
+        categories={["category1"]}
+        response={texts}
+        subcategories={["subcategory1"]}
+      />
+    );
   } catch (error: unknown) {
     return <div>error {JSON.stringify(error)}</div>;
   }
-  return (
-    <DashboardSectionOrganisms
-      categories={["category1"]}
-      response={{
-        message: "",
-        statusCode: 0,
-        data: {
-          id1: {
-            category: "category1",
-            subcategory: "subcategory1",
-            name: "name1",
-            id: "id1",
-            description: "description1",
-          },
-        },
-      }}
-      subcategories={["subcategory1"]}
-    />
-  );
 }
